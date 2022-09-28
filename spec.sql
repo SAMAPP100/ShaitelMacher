2cca1fbe085b5e4b5c2e396b4210c059f86a4dfb
S Breuer <support@app100.co.uk>
2022-09-28 18:35:14 -0400

    initial commit

---
+++ spec.sql
@@ -0,0 +1,45 @@
+/*
+
+Hi, I am a shaitel macher for some time now, and things have gotten very dissorganized over the years. 
+It was time for a program to keep my place in order.
+
+I need to keep track of all my appointments and the profit earned on each appointment, on some appointments I sell a shaitel 
+so I also need to record the price of the shaitel.
+
+I need to be able to generate the following reports:
+
+1) A list of all appointments for the day, in the following format
+    Row for the day
+    Time, First name, Last name, If they're buying a new shitle, How much they need to pay
+
+2) A report with the profit for every week/month
+3) A report withthe profit on sales, and for appointments separately
+4) I need a report of profit per shaitel brand
+5) How much I need to pay all the shaitel suppliers
+
+
+Question: Can someone buy a new shaitel at the same time style an old one?
+Answer: They can but that would be two appointments in the system.
+ 
+Question: Can an appointment be canceled?
+Answer: Yes upto 24 hours before, otherwise they have to pay.
+
+Question: How much in advanced can an appointment be booked?
+Answer: Upto 6 month in advance.
+
+If you have any more questions please reach out to me.
+
+Sample data:
+
+Tzippy Berger appt 12:30 tue 10/12/22 , New shaitel for $1200 it costed $900.
+Sarah Blau appt 1:30  10/12/22, Style $40.
+Gitty Gold appt 11:00 10/13/22, Double appointment for two styles for $80
+Zahava Green appt 12:00 10/13/22, New shaitel for $1300 it costed $1000, than she canceled.
+Chumi Schwartz appt 9:00 10/14/22, Style $40.
+Leah Roth appt 10:00 10/14/22, New shaitel for $1100 it costed $800.
+
+
+
+
+
+*/
