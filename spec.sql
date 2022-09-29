3c3737cdf9be0501e56c2f86f12103ca512b6f6d
S Breuer <support@app100.co.uk>
2022-09-29 13:20:42 -0400

    spec

--- spec.sql
+++ spec.sql
@@ -6,6 +6,8 @@ It was time for a program to keep my place in order.
 I need to keep track of all my appointments and the profit earned on each appointment, on some appointments I sell a shaitel 
 so I also need to record the price of the shaitel.
 
+Appointments are between 9 to 5 and are every half an hour 00 and 30.
+
 I need to be able to generate the following reports:
 
 1) A list of all appointments for the day, in the following format
@@ -13,7 +15,7 @@ I need to be able to generate the following reports:
     Time, First name, Last name, If they're buying a new shitle, How much they need to pay
 
 2) A report with the profit for every week/month
-3) A report withthe profit on sales, and for appointments separately
+3) A report with the profit on sales, and for appointments separately
 4) I need a report of profit per shaitel brand
 5) How much I need to pay all the shaitel suppliers
 
@@ -25,18 +27,25 @@ Question: Can an appointment be canceled?
 Answer: Yes upto 24 hours before, otherwise they have to pay.
 
 Question: How much in advanced can an appointment be booked?
-Answer: Upto 6 month in advance.
+Answer: Minimum 48 hours Upto 6 month in advance.
+
+Question: When someone buys a new shaitel do they also pay for the appointment?
+Answer: No its all inclusive in the price. But if they cancel with in 24 hours then they have to pay $40 for the appointment. 
+
+Question: When someone buys a shaitel is the price and brand known before hand?
+Answer: Yes the appointment is for cutting and styling. Choosing the shaitel happens before hand without an appointment.
+
 
 If you have any more questions please reach out to me.
 
 Sample data:
 
-Tzippy Berger appt 12:30 tue 10/12/22 , New shaitel for $1200 it costed $900.
+Tzippy Berger appt 12:30 tue 10/12/22 , New TheWigCo shaitel for $1200 it costed $900.
 Sarah Blau appt 1:30  10/12/22, Style $40.
 Gitty Gold appt 11:00 10/13/22, Double appointment for two styles for $80
-Zahava Green appt 12:00 10/13/22, New shaitel for $1300 it costed $1000, than she canceled.
-Chumi Schwartz appt 9:00 10/14/22, Style $40.
-Leah Roth appt 10:00 10/14/22, New shaitel for $1100 it costed $800.
+Zahava Green appt 12:00 10/13/22, New LuckyMe shaitel for $1300 it costed $1000.
+Chumi Schwartz appt 9:00 10/14/22, Style $40, than she canceled.
+Leah Roth appt 10:00 10/14/22, New TheWigCo shaitel for $1100 it costed $800.
 
 
 
