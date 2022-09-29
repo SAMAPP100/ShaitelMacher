82a25262b84d5f20151422daebf0d38140fcbd6a
S Breuer <support@app100.co.uk>
2022-09-29 18:44:11 -0400

    Fix spelling

--- spec.sql
+++ spec.sql
@@ -1,10 +1,10 @@
 /*
 
-Hi, I am a shaitel macher for some time now, and things have gotten very dissorganized over the years. 
+Hi, I am a sheitel macher for some time now, and things have gotten very disorganized over the years. 
 It was time for a program to keep my place in order.
 
-I need to keep track of all my appointments and the profit earned on each appointment, on some appointments I sell a shaitel 
-so I also need to record the price of the shaitel.
+I need to keep track of all my appointments and the profit earned on each appointment, on some appointments I sell a sheitel 
+so I also need to record the price of the sheitel.
 
 Appointments are between 9 to 5 and are every half an hour 00 and 30.
 
@@ -12,40 +12,40 @@ I need to be able to generate the following reports:
 
 1) A list of all appointments for the day, in the following format
     Row for the day
-    Time, First name, Last name, If they're buying a new shitle, How much they need to pay
+    Time, First name, Last name, If they're buying a new sheitel, How much they need to pay
 
 2) A report with the profit for every week/month
 3) A report with the profit on sales, and for appointments separately
-4) I need a report of profit per shaitel brand
-5) How much I need to pay all the shaitel suppliers
+4) I need a report of profit per sheitel brand
+5) How much I need to pay all the sheitel suppliers
 
 
-Question: Can someone buy a new shaitel at the same time style an old one?
-Answer: They can but that would be two appointments in the system.
+Question: Can someone buy a new sheitel at the same time style an old one? Can someone style two sheitels at the same appointment?
+Answer: They can but that would be two appointments in the system, so that we can keep track of the available time.
  
 Question: Can an appointment be canceled?
-Answer: Yes upto 24 hours before, otherwise they have to pay.
+Answer: Yes up to 24 hours before, otherwise they have to pay.
 
 Question: How much in advanced can an appointment be booked?
-Answer: Minimum 48 hours Upto 6 month in advance.
+Answer: Minimum 48 hours up to 6 month in advance.
 
-Question: When someone buys a new shaitel do they also pay for the appointment?
-Answer: No its all inclusive in the price. But if they cancel with in 24 hours then they have to pay $40 for the appointment. 
+Question: When someone buys a new sheitel do they also pay for the appointment?
+Answer: No it's all inclusive in the price. But if they cancel with in 24 hours then they have to pay $40 for the appointment. 
 
-Question: When someone buys a shaitel is the price and brand known before hand?
-Answer: Yes the appointment is for cutting and styling. Choosing the shaitel happens before hand without an appointment.
+Question: When someone buys a sheitel is the price and brand known before hand?
+Answer: Yes the appointment is for cutting and styling. Choosing the sheitel happens before hand without an appointment.
 
 
 If you have any more questions please reach out to me.
 
 Sample data:
 
-Tzippy Berger appt 12:30 tue 10/12/22 , New TheWigCo shaitel for $1200 it costed $900.
-Sarah Blau appt 1:30  10/12/22, Style $40.
-Gitty Gold appt 11:00 10/13/22, Double appointment for two styles for $80
-Zahava Green appt 12:00 10/13/22, New LuckyMe shaitel for $1300 it costed $1000.
-Chumi Schwartz appt 9:00 10/14/22, Style $40, than she canceled.
-Leah Roth appt 10:00 10/14/22, New TheWigCo shaitel for $1100 it costed $800.
+Tzippy Berger appt 12:30 tue 10/12/22 , New, TheWigCo sheitel for $1200 it cost $900.
+Sarah Blau appt 1:30  10/12/22, Style, $40.
+Gitty Gold appt 11:00 10/13/22, Double appointment for two styles, for $80
+Zahava Green appt 12:00 10/13/22, New, LuckyMe sheitel for $1300 it cost $1000.
+Chumi Schwartz appt 9:00 10/14/22, Style, $40, than she canceled.
+Leah Roth appt 10:00 10/14/22, New, TheWigCo sheitel for $1100 it cost $800.
 
 
 
